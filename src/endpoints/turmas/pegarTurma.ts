8de94343c0df8083b6dbf2f8a74e3ddc1e6dd3af
Leonardo Martins <fmartins.leonardo@gmail.com>
2021-12-09 16:56:39 -0300

    pegando as turmas

---
+++ src/endpoints/turmas/pegarTurma.ts
@@ -0,0 +1,13 @@
+import { connection } from "../../connection";
+import { Request, Response } from "express";
+import { Turma } from "../../types";
+
+export const pegarTurma = async (req: Request, res: Response) => {
+  try{
+    const turmas:Turma[] = await connection("labesystem_turma")
+    res.send(turmas)
+
+  } catch (error: any) {
+    res.status(400). send({message: error.message})
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import {app} from "./app";
 import { cadastrarDocente } from "./endpoints/docentes/cadastrarDocente";
 import { cadastrarEstudante } from "./endpoints/estudantes/cadastrarEstudante";
 import { buscarEstudante } from "./endpoints/estudantes/buscarEstudante";
-import { criarTurma } from "./endpoints/criarTurma";
+import { criarTurma } from "./endpoints/turmas/criarTurma";
 import { testeInicial } from "./endpoints/testeInicial";
 import { mudarEstudante } from "./endpoints/estudantes/mudarEstudante";
 import { buscarDocente } from "./endpoints/docentes/buscarDocentes";
 import { mudarDocente } from "./endpoints/docentes/mudarDocente";
+import { pegarTurma } from "./endpoints/turmas/pegarTurma";
 
 app.get("/", testeInicial)
 
 app.post("/turma", criarTurma)
+app.get("/turma", pegarTurma)
 
 
 
@@ -26,5 +28,3 @@ app.put("/estudantes/:id", mudarEstudante)
 app.put("/docentes", cadastrarDocente)
 app.get("/docentes", buscarDocente)
 app.put("/docentes/:id", mudarDocente)
-
-
